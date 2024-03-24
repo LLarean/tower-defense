3898e94748b58e7991678577232dfd877927d349
Stanislav Kozlov <llarean@ya.ru>
2024-03-24 12:11:04 +0300

    Build Settings
    Terrain textures have been added.
    Configuring the terrain for the build.

--- Assets/Scripts/UI/Menu/MainMenu.cs
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -7,15 +7,22 @@ namespace UI.Menu
     public class MainMenu : MonoBehaviour
     {
         [SerializeField] private Button _start;
+        [SerializeField] private Button _exit;
 
         private void Start()
         {
             _start.onClick.AddListener(LoadGameScene);
+            _exit.onClick.AddListener(Exit);
         }
 
         private void LoadGameScene()
         {
             SceneManager.LoadScene(GlobalStrings.Demo);
+        }
+
+        private void Exit()
+        {
+            Application.Quit();
         }
     }
 }
