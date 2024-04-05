aa7f3b46ea8c931a420361f19075f398708d65f0
Stanislav Kozlov <llarean@ya.ru>
2024-04-05 20:13:03 +0300

    Updating the work of effects and tests for this

--- Assets/Scripts/Test/PlayMode/UnitDebuffTests.cs
+++ Assets/Scripts/Test/PlayMode/UnitDebuffTests.cs
@@ -73,79 +73,176 @@ namespace Test
             Assert.IsTrue(unitEffects.DebuffModels[1].DebuffType == DebuffType.Intoxication);
         }
         
-        // [Test]
-        // public void TakeCopyEffect()
-        // {
-        //     var effect = ElementalType.Fire;
-        //     var unitEffects = new UnitEffects();
-        //     unitEffects.TakeEffect(effect);
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     Assert.IsTrue(unitEffects.ActiveEffects.Count == 1);
-        // }
-        //
-        //
-        // [Test]
-        // public void TakeWaterIceEffects()
-        // {
-        //     var unitEffects = new UnitEffects();
-        //
-        //     var effect = ElementalType.Water;
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     effect = ElementalType.Ice;
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     Assert.IsTrue(unitEffects.ActiveEffects.Count == 2);
-        //     Assert.IsTrue(unitEffects.DebuffModels[0].DebuffType == DebuffType.Frozen);
-        // }
-        //
-        // [Test]
-        // public void TakeWaterIceFireEffects()
-        // {
-        //     var unitEffects = new UnitEffects();
-        //
-        //     var effect = ElementalType.Water;
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     effect = ElementalType.Ice;
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     effect = ElementalType.Fire;
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     Assert.IsTrue(unitEffects.DebuffModels.Count == 0);
-        //     Assert.IsTrue(unitEffects.ActiveEffects.Count == 0);
-        // }
-        //
-        // [Test]
-        // public void TakeFireIceEffects()
-        // {
-        //     var unitEffects = new UnitEffects();
-        //
-        //     var effect = ElementalType.Fire;
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     effect = ElementalType.Ice;
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     Assert.IsTrue(unitEffects.DebuffModels.Count == 0);
-        //     Assert.IsTrue(unitEffects.ActiveEffects.Count == 0);
-        // }
-        //
-        // [Test]
-        // public void TakeFireWaterEffects()
-        // {
-        //     var unitEffects = new UnitEffects();
-        //
-        //     var effect = ElementalType.Fire;
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     effect = ElementalType.Water;
-        //     unitEffects.TakeEffect(effect);
-        //
-        //     Assert.IsTrue(unitEffects.DebuffModels.Count == 0);
-        //     Assert.IsTrue(unitEffects.ActiveEffects.Count == 0);
-        // }
+        [Test]
+        public void TakeFireWaterEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Fire;
+            unitEffects.TakeEffect(effect);
+        
+            effect = ElementalType.Water;
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 0);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 0);
+        }
+        
+        [Test]
+        public void TakeFireIceEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Fire;
+            unitEffects.TakeEffect(effect);
+        
+            effect = ElementalType.Ice;
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 0);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 0);
+        }
+        
+        [Test]
+        public void TakeFireFireFireEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Fire;
+            unitEffects.TakeEffect(effect);
+            unitEffects.TakeEffect(effect);
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels[0].DebuffType == DebuffType.Burning);
+        }
+        
+        [Test]
+        public void TakePoisonPoisonPoisonEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Poison;
+            unitEffects.TakeEffect(effect);
+            unitEffects.TakeEffect(effect);
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels[0].DebuffType == DebuffType.Intoxication);
+        }
+        
+        [Test]
+        public void TakeWaterWaterWaterEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Water;
+            unitEffects.TakeEffect(effect);
+            unitEffects.TakeEffect(effect);
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels[0].DebuffType == DebuffType.Wet);
+        }
+        
+        [Test]
+        public void TakeIceIceIceEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Ice;
+            unitEffects.TakeEffect(effect);
+            unitEffects.TakeEffect(effect);
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels[0].DebuffType == DebuffType.Slow);
+        }
+        
+        [Test]
+        public void TakeFireIceFireEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Fire;
+            unitEffects.TakeEffect(effect);
+            effect = ElementalType.Ice;
+            unitEffects.TakeEffect(effect);
+            effect = ElementalType.Fire;
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels[0].DebuffType == DebuffType.Burning);
+        }
+        
+        [Test]
+        public void TakeFirePoisonFirePoisonEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Fire;
+            unitEffects.TakeEffect(effect);
+            effect = ElementalType.Poison;
+            unitEffects.TakeEffect(effect);
+            effect = ElementalType.Fire;
+            unitEffects.TakeEffect(effect);
+            effect = ElementalType.Poison;
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 2);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 2);
+            Assert.IsTrue(unitEffects.DebuffModels[0].DebuffType == DebuffType.Burning);
+            Assert.IsTrue(unitEffects.DebuffModels[1].DebuffType == DebuffType.Intoxication);
+        }
+        
+        [Test]
+        public void TakeIceFireIceEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Ice;
+            unitEffects.TakeEffect(effect);
+            effect = ElementalType.Fire;
+            unitEffects.TakeEffect(effect);
+            effect = ElementalType.Ice;
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 1);
+            Assert.IsTrue(unitEffects.DebuffModels[0].DebuffType == DebuffType.Slow);
+        }
+        
+        [Test]
+        public void TakePoisonWaterEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Poison;
+            unitEffects.TakeEffect(effect);
+            effect = ElementalType.Water;
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 0);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 0);
+        }
+        
+        [Test]
+        public void TakeWaterPoisonEffects()
+        {
+            var unitEffects = new UnitEffects();
+        
+            var effect = ElementalType.Water;
+            unitEffects.TakeEffect(effect);
+            effect = ElementalType.Poison;
+            unitEffects.TakeEffect(effect);
+        
+            Assert.IsTrue(unitEffects.ActiveEffects.Count == 0);
+            Assert.IsTrue(unitEffects.DebuffModels.Count == 0);
+        }
     }
 }
--- Assets/Scripts/Units/UnitEffects.cs
+++ Assets/Scripts/Units/UnitEffects.cs
@@ -65,7 +65,11 @@ public class UnitEffects
                     removedEffects.Add(activeEffect);
                     break;
                 case ElementalType.Fire when effectModel.ElementalType == ElementalType.Water:
+                case ElementalType.Poison when effectModel.ElementalType == ElementalType.Water:
                 case ElementalType.Fire when effectModel.ElementalType == ElementalType.Ice:
+                    removedEffects.Add(activeEffect);
+                    break;
+                case ElementalType.Water when effectModel.ElementalType == ElementalType.Poison:
                     removedEffects.Add(activeEffect);
                     break;
             }
@@ -96,108 +100,82 @@ public class UnitEffects
             return;
         }
         
-        bool canAddDebuff;
-        
-        DebuffModel debuffModel = new DebuffModel()
-        {
-            Duration = GlobalParams.DebuffDuration,
-        };
-        
-        canAddDebuff = TryGetDebuffBasedTwoEffects(in debuffModel);
+        var debuffModels = new List<DebuffModel>();
+        debuffModels = GetDebuffsBasedTwoEffects(debuffModels);
 
-        if (canAddDebuff == false)
+        if (debuffModels.Count == 0)
         {
-            canAddDebuff = TryGetDebuffBasedEffect(in debuffModel);
+            debuffModels = GetDebuffsBasedEffect(debuffModels);
         }
         
-        if (canAddDebuff == true)
+        if (debuffModels.Count != 0)
         {
-            RemoveConflictingDebuffs(debuffModel);
-            _debuffModels.Add(debuffModel);
+            _debuffModels = debuffModels;
         }
     }
 
-    private bool TryGetDebuffBasedTwoEffects(in DebuffModel debuffModel)
+    private List<DebuffModel> GetDebuffsBasedTwoEffects(List<DebuffModel> debuffModels)
     {
-        var isSuccess = false;
-
+        var debuffModel = new DebuffModel();
+        
         foreach (var activeEffect in _activeEffects)
         {
             foreach (var effect in _activeEffects)
             {
                 if (activeEffect.ElementalType == ElementalType.Water && effect.ElementalType == ElementalType.Ice)
                 {
-                    debuffModel.DebuffType = DebuffType.Frozen;
-                    isSuccess = true;
-                    return isSuccess;
+                    debuffModel = GetDebuffModel(DebuffType.Frozen);
+                    debuffModels.Add(debuffModel);
                 }
             
                 if (activeEffect.ElementalType == ElementalType.Ice && effect.ElementalType == ElementalType.Water)
                 {
-                    debuffModel.DebuffType = DebuffType.Frozen;
-                    isSuccess = true;
-                    return isSuccess;
+                    debuffModel = GetDebuffModel(DebuffType.Frozen);
+                    debuffModels.Add(debuffModel);
                 }
             }
         }
-        
-        return isSuccess;
+
+        return debuffModels;
     }
-    
-    private bool TryGetDebuffBasedEffect(in DebuffModel debuffModel)
+
+    private DebuffModel GetDebuffModel(DebuffType debuffType)
     {
-        var isSuccess = false;
-        
+        var debuffModel = new DebuffModel()
+        {
+            Duration = GlobalParams.DebuffDuration,
+            DebuffType = debuffType,
+        };
+
+        return debuffModel;
+    }
+
+    private List<DebuffModel> GetDebuffsBasedEffect(List<DebuffModel> debuffModels)
+    {
+        var debuffModel = new DebuffModel();
+            
         foreach (var activeEffect in _activeEffects)
         {
             if (activeEffect.ElementalType == ElementalType.Fire)
             {
-                debuffModel.DebuffType = DebuffType.Burning;
-                isSuccess = true;
-                return isSuccess;
+                debuffModel = GetDebuffModel(DebuffType.Burning);
+            }
+            else if (activeEffect.ElementalType == ElementalType.Poison)
+            {
+                debuffModel = GetDebuffModel(DebuffType.Intoxication);
+            }
+            else if (activeEffect.ElementalType == ElementalType.Water)
+            {
+                debuffModel = GetDebuffModel(DebuffType.Wet);
+            }
+            else if (activeEffect.ElementalType == ElementalType.Ice)
+            {
+                debuffModel = GetDebuffModel(DebuffType.Slow);
             }
             
-            if (activeEffect.ElementalType == ElementalType.Poison)
-            {
-                debuffModel.DebuffType = DebuffType.Intoxication;
-                isSuccess = true;
-                return isSuccess;
-            }
-            
-            if (activeEffect.ElementalType == ElementalType.Water)
-            {
-                debuffModel.DebuffType = DebuffType.Wet;
-                isSuccess = true;
-                return isSuccess;
-            }
-            
-            if (activeEffect.ElementalType == ElementalType.Ice)
-            {
-                debuffModel.DebuffType = DebuffType.Slow;
-                isSuccess = true;
-                return isSuccess;
-            }
+            debuffModels.Add(debuffModel);
         }
 
-        return isSuccess;
-    }
-    
-    private void RemoveConflictingDebuffs(DebuffModel debuffModel)
-    {
-        var conflictingDebuffs = new List<DebuffModel>();
-        
-        foreach (var model in _debuffModels)
-        {
-            switch (model.DebuffType)
-            {
-                case DebuffType.Wet when debuffModel.DebuffType == DebuffType.Frozen:
-                case DebuffType.Slow when debuffModel.DebuffType == DebuffType.Frozen:
-                    conflictingDebuffs.Add(model);
-                    break;
-            }
-        }
-        
-        _debuffModels = _debuffModels.Except(conflictingDebuffs).ToList();
-
+        return debuffModels;
     }
 }
