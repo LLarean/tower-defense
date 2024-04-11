6aa0fdc9f92668ab575c4a4f0a20ccc600b9ce38
Stanislav Kozlov <llarean@ya.ru>
2024-04-11 15:08:03 +0300

    The model of the tower

---
+++ Assets/Scripts/Globals/GlobalColors.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public static class GlobalColors
+{
+    // #f77703
+    public static Color32 Fire = new Color32
+    {
+        r = 247,
+        g = 119,
+        b = 3,
+        a = 255
+    };
+
+    // #61de2a
+    public static Color32 ToxicGreen = new Color32
+    {
+        r = 97,
+        g = 222,
+        b = 42,
+        a = 255
+    };
+
+    // #266691
+    public static Color32 Water = new Color32
+    {
+        r = 38,
+        g = 102,
+        b = 145,
+        a = 255
+    }; 
+
+    // #6a737b
+    public static Color32 Ice = new Color32
+    {
+        r = 106,
+        g = 115,
+        b = 123,
+        a = 255
+    };
+}
